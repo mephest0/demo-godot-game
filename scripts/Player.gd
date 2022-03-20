0254cfc6c198f810a2a8d44c6e98da60e6ec5ff5
mephest0 <peter@vev.design>
2022-03-20 21:51:53 +0100

    Added jumping

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,7 +2,10 @@ extends KinematicBody2D
 
 
 const SPEED = 10
+const JUMP = Vector2(0, -100)
+
 const G = Vector2(0, 2)
+const UP = Vector2(0, -1)
 
 var movement = Vector2()
 
@@ -14,5 +17,8 @@ func _physics_process(delta):
 		movement.x -= SPEED
 	if Input.is_action_pressed("move_right"):
 		movement.x += SPEED
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		movement += JUMP
 	
-	movement = move_and_slide(movement)
+	movement = move_and_slide(movement, UP)
